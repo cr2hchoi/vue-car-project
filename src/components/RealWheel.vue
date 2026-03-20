241611cefa5327bf54e6b2471249c283bab4d120
cr2hChoi <20231046@sungshin.ac.kr>
2026-03-20 15:46:57 +0900

    [#2]feat: FrontWheel 및 RealWheel 컴포넌트 작성

---
+++ src/components/RealWheel.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Tire from "./Tire.vue";
+</script>
+
+
+<template>
+  <div class="real-wheel">
+    <span>Front Wheel</span>
+    <Tire color = "PURPLE"/>
+    <!--    Tire 컬러 대입-->
+  </div>
+</template>
+
+<style scoped>
+.real-wheel{
+  width: 150px;
+  height: 150px;
+  border: 4px solid #333; /* 휠 프레임 두께와 색상 */
+  border-radius: 50%;     /* 완벽한 원형 만들기 */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f8f9fa;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+}
+</style>
